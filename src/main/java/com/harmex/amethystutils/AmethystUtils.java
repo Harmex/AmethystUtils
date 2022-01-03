e6e69a499ef80ae5f6399d654d7effcbf01407d7
Harmex <cyril.romand98@gmail.com>
2022-01-03 15:37:25 +0100

    added resources

--- src/main/java/com/harmex/amethystutils/AmethystUtils.java
+++ src/main/java/com/harmex/amethystutils/AmethystUtils.java
@@ -4,16 +4,10 @@ import com.harmex.amethystutils.common.block.ModBlocks;
 import com.harmex.amethystutils.common.block.entity.ModBlockEntityTypes;
 import com.harmex.amethystutils.common.inventory.ModMenuTypes;
 import com.harmex.amethystutils.common.item.ModItems;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -37,10 +31,6 @@ public class AmethystUtils
 
         // Register the setup method for modloading
         eventBus.addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        eventBus.addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        eventBus.addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -49,32 +39,5 @@ public class AmethystUtils
     private void setup(final FMLCommonSetupEvent event)
     {
 
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-
-    }
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {
-        // do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
